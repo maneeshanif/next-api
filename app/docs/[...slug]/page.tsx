4d233fb28ad1eb7db1c7ce508c2025878ea7b22b
M.Anees <manees7865@gmail.com>
2025-01-05 19:13:29 +0500

    catch all segment

---
+++ app/docs/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-anees-200 sm:text-blue-300 md:text-green-300 lg:text-red-500 dark:bg-red-300 dark:text-white bg-yellow-200 block text-center font-bold underline'>this is a example of cathc all segment no need of more nested dynamic route </h1>
+    </div>
+  )
+}
+
+export default page
